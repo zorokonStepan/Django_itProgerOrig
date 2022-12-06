69a0a75ceddfb940e3f9bf4724c6b0da0ba2d779
Stepan Kolodkin <zorokon@yandex.ru>
2022-12-06 18:17:13 +0300

    add all

--- example_site/main/urls.py
+++ example_site/main/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+
+    path('index_2/', views.index_2)
 ]
--- example_site/main/views.py
+++ example_site/main/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
+# HttpResponse подходит для вывода не большого количества информации
 def index(request):
     return HttpResponse('<h1><center>Главная страница</center></h1>')
 
@@ -12,3 +14,6 @@ def about(request):
 def contact(request):
     return HttpResponse('<h1><center>Контакты</center></h1>')
 
+
+def index_2(request):
+    return render(request, 'main/index_2.html')

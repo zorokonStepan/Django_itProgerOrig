0d578d88ee8bfc6fe9c6dbf3c8fcd6236befb092
Stepan Kolodkin <zorokon@yandex.ru>
2022-12-07 16:29:19 +0300

    add templates

--- example_site/main/urls.py
+++ example_site/main/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('index_3/', views.index_3, name='index_3'),
+    path('about_3/', views.about_3, name='about_3'),
+    path('contact_3/', views.contact_3, name='contact_3'),
 
-    path('index_2/', views.index_2),
-    path('about_2/', views.about_2),
-    path('contact_2/', views.contact_2),
+    path('index_2/', views.index_2, name='index_2'),
+    path('about_2/', views.about_2, name='about_2'),
+    path('contact_2/', views.contact_2, name='contact_2'),
+
+    path('index_1/', views.index_1),
+    path('about_1/', views.about_1),
+    path('contact_1/', views.contact_1),
 ]
--- example_site/main/views.py
+++ example_site/main/views.py
@@ -2,28 +2,49 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'main/index.html')
+def index_3(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['123', 'stroka', True],
+        'obj': {
+            'car': 'BMV',
+            'age': 18,
+            'hobby': 'football'
+        }
+    }
+    return render(request, 'main/index_3.html', data)
 
 
-def about(request):
-    return render(request, 'main/about.html')
+def about_3(request):
+    return render(request, 'main/about_3.html')
 
 
-def contact(request):
-    return render(request, 'main/index.html')
+def contact_3(request):
+    return render(request, 'main/index_3.html')
 
 
-# HttpResponse подходит для вывода не большого количества информации
 def index_2(request):
-    return HttpResponse('<h1><center>Главная страница</center></h1>')
+    return render(request, 'main/index_2.html')
 
 
 def about_2(request):
-    return HttpResponse('<h1><center>О нас</center></h1>')
+    return render(request, 'main/about_2.html')
 
 
 def contact_2(request):
+    return render(request, 'main/index_2.html')
+
+
+# HttpResponse подходит для вывода не большого количества информации
+def index_1(request):
+    return HttpResponse('<h1><center>Главная страница</center></h1>')
+
+
+def about_1(request):
+    return HttpResponse('<h1><center>О нас</center></h1>')
+
+
+def contact_1(request):
     return HttpResponse('<h1><center>Контакты</center></h1>')
 
 
